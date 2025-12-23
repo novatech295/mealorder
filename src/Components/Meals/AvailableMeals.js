f83d5f3e33a22eb4afc08865507f91bf932e0424
novatech295 <novatech295@gmail.com>
2025-12-23 20:50:46 +0300

    Das

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -1,51 +1,36 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { CounterContext } from '../../App';
 import '../Header/Header.css';
-import axios from 'axios';
+import { MEALS_DATA } from '../../data/mealsData';
+
 const AvailableMeals = () => {
-  const [Demmy_Meals, setDemmy_Meals] = useState([]);
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
   const { setCount } = useContext(CounterContext);
-  useEffect(() => {
+  const [Demmy_Meals] = useState(MEALS_DATA.items);
 
-    setIsLoading(true);
-    axios.get('https://nehadshretahh.pythonanywhere.com/items/')
-      .then((response => {
-  
-        setDemmy_Meals(response.data.items)
-      })).catch((error) => {
-        setError(error);
-      }).finally(() => {
-        setIsLoading(false);
-      });
-
-  }, []);
-  if (isLoading) {
-    return (<div>Loading...</div>);
-  }
-  if (error) {
-    return( <div>error : {error.message}</div>);
-  }
   var root = document.querySelector(':root');
   root.style.setProperty('--visibility', 'visible');
   root.style.setProperty('--heightImg', '300px');
+
   const Add = (id) => {
     setCount((count) => [...count, id]);
   };
+
   const meallist = Demmy_Meals.map((meal) => (
     <div className='row li' key={meal.id}>
-      <div className='col-10  col-sm-9   '>
+      <div className='col-10 col-sm-9'>
         <h4>{meal.name}</h4>
         <p>{meal.description}</p>
         <h4 className='price'>${meal.price}</h4>
         <hr className="my-hr" />
       </div>
-      <div className='col-2 col-sm-3   '>
-        <button className='add-btn' onClick={() => Add(meal.id)}>+ Add</button>
+      <div className='col-2 col-sm-3'>
+        <button className='add-btn' onClick={() => Add(meal.id)}>
+          + Add
+        </button>
       </div>
     </div>
   ));
+
   return (
     <div className='ava'>
       <div className='container'>
---
+++ src/data/mealsData.js
@@ -0,0 +1,28 @@
+export const MEALS_DATA = {
+  items: [
+    {
+      id: 1,
+      name: "SuShi",
+      price: 20.59,
+      description: "Some Text and Some Text",
+    },
+    {
+      id: 2,
+      name: "Fish",
+      price: 25.19,
+      description: "Some Text and Some Text",
+    },
+    {
+      id: 3,
+      name: "Pizza",
+      price: 22.09,
+      description: "Some Text and Some Text",
+    },
+    {
+      id: 4,
+      name: "Chicken",
+      price: 40.29,
+      description: "Some Text and Some Text",
+    },
+  ],
+};
